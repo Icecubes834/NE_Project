57bc5011c9bb0484ec2ba984aa33ed1775567c6a
Icecubes834 <icecubes834@gmail.com>
2025-07-06 18:41:03 +0100

    4th version of main.cpp
    Fully functional version and the last version before splitted into header files.
    
    Created: 11/05/2025 19:09
    Last modified: 12/05/2025 14:05

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <SDL.h>
-#include <ctime>
 #include <unistd.h>
 #include <vector>
 #include <time.h>
-#include <sstream>
+#include <SDL_ttf.h>
 
 using namespace std;
 
@@ -12,52 +11,25 @@ bool init();
 void kill();
 bool loop();
 void changeState(int padNumber);
+char assignIntToHex(int inputNumber);
+void drawText(const char* inputText,int x, int y);
+bool checkIfBinaryNumberEqualsHex(int inputBinaryNumber[8],char inputHexData[2]);
+bool check4BitEqualsHex(int inputBinaryNumber[4],char inputHexData);
+void createMissile();
+Uint32 addEnemy(Uint32,void*);
+void createEnemy();
+
 
 // Pointers to our window and renderer
 SDL_Window* window;
 SDL_Renderer* renderer;
-SDL_Rect r;
+TTF_Font* font;
 
 //Defining the width and height of the screen
 const int WIDTH = 640;
 const int HEIGHT = 480;
-const int SCREEN_FPS = 60;
-const int SCREEN_TICK_PER_FRAME = 1000 / SCREEN_FPS;
 
-int binaryNumber[8] = {0,0,0,0,    0,0,0,0};
-
-
-
-class LTimer
-{
-    public:
-		//  Initializes variables
-		LTimer();
-
-		//  The various clock actions
-		void start();
-		void stop();
-		void pause();
-		void unpause();
-
-		//  Gets the timer's time
-		Uint32 getTicks();
-
-		//  Checks the status of the timer
-		bool isStarted();
-		bool isPaused();
-
-    private:
-		//  The clock time when the timer started
-		Uint32 mStartTicks;
-
-		//  The ticks stored when the timer was paused
-		Uint32 mPausedTicks;
-
-		//  The timer status
-		bool mPaused;
-		bool mStarted;
-};
+int binaryNumber[8] = {0,0,0,0,0,0,0,0};
 
 class enemy{
 	public:
@@ -66,17 +38,38 @@ class enemy{
 		//A function to draw the enemy
 		void draw();
 		
+		char* getHexData(){
+			return hexData;
+		}
+		
+		int getPosX(){
+			return enemyRect.x;
+		}
+		
+		SDL_Rect* getRect(){
+			return &enemyRect;
+		}
+		
+		bool defeated;
+		
 	private:
 		//Defines the shape of the enemy
 		SDL_Rect enemyRect;
 		//Shows the hex value it represents in a string format
 		char hexData[2];
-		
-		
 };
 
 
 class BinaryPad{
+	private:
+		SDL_Rect padRect;
+		
+		int state = 0;
+	
+		//The pad number it represents
+		int padNumber;
+	
+	
 	public:
 		//Constructor with starting pos x and pad number as its parameters
 		BinaryPad(int startingX,int inputPadNumber);
@@ -97,65 +90,37 @@ class BinaryPad{
 		void setState(int inputState){
 			state  = inputState;
 		}
+		
 	
-	private:
-		SDL_Rect padRect;
-		int state = 0;
-		//The pad number it represents
-		int padNumber;
 	
 };
 
-
-BinaryPad p1(0,0);
-BinaryPad p2(0,1);
-BinaryPad p3(0,2);
-BinaryPad p4(0,3);
-
-BinaryPad p5(0,4);
-BinaryPad p6(0,5);
-BinaryPad p7(0,6);
-BinaryPad p8(0,7);
-
+struct Missile{
+	SDL_Rect missileRect;
+	void draw(){
+		SDL_SetRenderDrawColor( renderer, 255, 255, 0, 255 );
+		missileRect.y = missileRect.y - 5;
+		SDL_RenderFillRect(renderer,&missileRect);
+	}
+	
+	SDL_Rect* getRect(){
+		return &missileRect;
+	}
+};
 
 vector<enemy> enemyVector;
 vector<BinaryPad> binaryPadVector;
-
-
-
-enemy e1;
+vector<Missile> missileSpriteVector;
 
 
 int main(int argc, char** args) {
-	srand(time(0));
+	
+	srand(time(NULL));
 	
 	if ( !init() ) return 1;
-	
-	LTimer fpsTimer;
-	LTimer capTimer;
-
-	int countedFrames = 0;
-	fpsTimer.start();
 
 	while ( loop() ) {
-		// wait before processing the next frame
-		capTimer.start();
-		
-		float avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f );
-		if ( avgFPS > 2000000 ){
-			avgFPS = 0;
-		}
-		
-		//cout <<"FPS: " <<avgFPS<<"\r";
-		
-		
-		int frameTicks = capTimer.getTicks();
-        if  ( frameTicks < SCREEN_TICK_PER_FRAME ){
-            // Wait remaining time
-			SDL_Delay( SCREEN_TICK_PER_FRAME - frameTicks );
-        }
-		
-		++countedFrames;
+		SDL_Delay(100);
 	}
 
 	kill();
@@ -167,14 +132,11 @@ bool loop() {
 	static const unsigned char* keys = SDL_GetKeyboardState( NULL );
 
 	SDL_Event e;
-	
+
 	
 	// Clear the window to white
 	SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
 	SDL_RenderClear( renderer );
-	
-	
-
 	
 	// Event loop
 	while ( SDL_PollEvent( &e ) != 0 ) {
@@ -197,7 +159,6 @@ bool loop() {
 					case SDLK_4:
 						changeState(3);
 						break;
-			
 					case SDLK_5: 
 						changeState(4);
 						break;
@@ -212,11 +173,9 @@ bool loop() {
 						break;
 						
 					case SDLK_SPACE:
-						enemy enemyBuffer;
-						enemyVector.push_back(enemyBuffer);
+						
+						
 						break;
-					
-				
 				}
 				break;
 		}
@@ -224,19 +183,27 @@ bool loop() {
 
 	
 
-	// Test key states - this could also be done with events
-	if ( keys[SDL_SCANCODE_2] ) r.x+=2;
+	// Test key states - this could also be done with events - Used for continuios input
+	if ( keys[SDL_SCANCODE_W] ) cout << "W was pressed"<<endl;
 	
-	if ( keys[SDL_SCANCODE_D] ) r.x += 2;
+	if ( keys[SDL_SCANCODE_A] ) cout << "A was pressed"<<endl;
 	
-	if ( keys[SDL_SCANCODE_W] ) r.y -= 2;
+	if ( keys[SDL_SCANCODE_S] ) cout << "S was pressed"<<endl;
 	
-	if ( keys[SDL_SCANCODE_S] ) r.y += 2;
+	if ( keys[SDL_SCANCODE_D] ) cout << "D was pressed"<<endl;
 	
-	// Set drawing color to Red
-	SDL_SetRenderDrawColor( renderer,0, 255, 0, 255 );
+	if (SDL_HasIntersection(missileSpriteVector[0].getRect(),enemyVector[0].getRect())){
+		enemyVector.erase(enemyVector.begin());
+		missileSpriteVector.erase(missileSpriteVector.begin());
+		createEnemy();
+	}
+	
 	for (uint8_t i = 0; i < enemyVector.size(); i++) {
 		enemyVector.at(i).draw();
+	}
+	
+	for (uint8_t i = 0; i < missileSpriteVector.size(); i++) {
+		missileSpriteVector.at(i).draw();
 	}
 	
 	//SDL_SetRenderDrawColor( renderer, 0,0, 255, 255 );
@@ -256,12 +223,20 @@ bool loop() {
 
 
 bool init() {
-	r.w = 50;
-	r.h = 50;
-	r.x = 0;
-	r.y = 0;
+	
+	enemy e1;
 	
 	enemyVector.push_back(e1);
+	
+	BinaryPad p1(0,0);
+	BinaryPad p2(0,1);
+	BinaryPad p3(0,2);
+	BinaryPad p4(0,3);
+
+	BinaryPad p5(0,4);
+	BinaryPad p6(0,5);
+	BinaryPad p7(0,6);
+	BinaryPad p8(0,7);
 	
 	binaryPadVector.push_back(p1);
 	binaryPadVector.push_back(p2);
@@ -296,11 +271,22 @@ bool init() {
 		cout << "Error creating renderer: " << SDL_GetError() << endl;
 		return false;
 	}
-
 	
+	if ( TTF_Init() < 0 ) {
+		cout << "Error intializing SDL_ttf: " << TTF_GetError() << endl;
+		return false;
+	}
+	
+	font = TTF_OpenFont("Fonts/Minecraft.ttf", 24);
+	if ( !font ) {
+		cout << "Failed to load font: " << TTF_GetError() << endl;
+	}
 
 	SDL_SetRenderDrawColor( renderer, 255, 255, 255, 255 );
 	SDL_RenderClear( renderer );
+	
+	//Add an enemy every 10 seconds
+	SDL_AddTimer(3000, addEnemy, nullptr);  
 	return true;
 }
 
@@ -312,51 +298,39 @@ void kill() {
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
 enemy::enemy(){
 	//Defining the width and height of the enemy and starting position
+	
+	defeated = false;
+	
 	enemyRect.w = 50;
 	enemyRect.h = 50;
 	
-	//Setting a seed based on time (for the random number generation)
-	//srand(time(0));
-	
-	//Generate a random number
-	int randomStartXPosition = 140 + (rand() % WIDTH -140);
-	
-	cout <<"Random Number generated: "<< randomStartXPosition << endl;
+	//Generate a random number between 0 and WIDTH - its width
+	int randomStartXPosition =  rand() % (WIDTH-enemyRect.w);
 	
 	enemyRect.x = randomStartXPosition;
-	enemyRect.y = 0;
+	enemyRect.y = -enemyRect.h - 25;
 	
 	int firstRandomHexDigit = rand()%16;
+	int secondRandomHexDigit = rand()%16;
 	
-	cout <<"The random number is: "<<firstRandomHexDigit;
-	if (firstRandomHexDigit > 9){
-		if(firstRandomHexDigit == 10) hexData[0] = 'A';
-		if(firstRandomHexDigit == 11) hexData[0] = 'B';
-		if(firstRandomHexDigit == 12) hexData[0] = 'C';
-		if(firstRandomHexDigit == 13) hexData[0] = 'D';
-		if(firstRandomHexDigit == 14) hexData[0] = 'E';
-		if(firstRandomHexDigit == 15) hexData[0] = 'F';
-	}
-	cout << "Hex number"<<hexData;
+	hexData[0] = assignIntToHex(firstRandomHexDigit);
+	hexData[1] = assignIntToHex(secondRandomHexDigit);
+	
+	for (int i = 0; i < 2; i++){
+		cout << hexData[i];
+	} 
+	
+	cout << endl;
 }
 
 void enemy::draw(){
 	enemyRect.y += 1;
-	
+	SDL_SetRenderDrawColor( renderer,0, 255, 0, 100 );
 	SDL_RenderFillRect(renderer,&enemyRect);
+	
+	drawText(hexData,enemyRect.x,enemyRect.y);
 }
 
 
@@ -366,7 +340,6 @@ void enemy::draw(){
 
 BinaryPad::BinaryPad(int startingX,int inputPadNumber){
 	//Constructor
-	
 	padRect.w = 50;
 	padRect.h = 50;
 	
@@ -384,6 +357,13 @@ void BinaryPad::draw(){
 		SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
 	}
 	SDL_RenderFillRect(renderer,&padRect);
+	
+	
+	if (state == 0){
+		drawText("0",padRect.x,padRect.y);
+	}else{
+		drawText("1",padRect.x,padRect.y);
+	}
 }
 
 void BinaryPad::changeX(int x){
@@ -403,120 +383,297 @@ void changeState(int padNumber){
 				binaryPadVector.at(i).setState(0);
 				binaryNumber[i] = 0;
 			}
-			cout << "Successfully changed state: " << binaryPadVector.at(i).getState() <<endl;
+			//cout << "Successfully changed state: " << binaryPadVector.at(i).getState() <<endl;
 			for (int i = 0;i < 8;i++){
-				cout << binaryNumber[i];
+				//cout << binaryNumber[i];
 			}
 		}
 	}
+	
+	if(checkIfBinaryNumberEqualsHex(binaryNumber,enemyVector[0].getHexData()) && enemyVector[0].defeated == false){
+		cout << "Got it correct" << endl;
+		createMissile();
+		enemyVector[0].defeated = true;
+	}
+	
+}
+
+bool check4BitEqualsHex(int fourBitBuffer[4],char inputHexData){
+	switch(inputHexData){
+		//Checking numbers
+		case '0':
+			if(fourBitBuffer[0] == 0 && fourBitBuffer[1] == 0 && fourBitBuffer[2] == 0 && fourBitBuffer[3] == 0 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '1':
+			if(fourBitBuffer[3] == 0 && fourBitBuffer[1] == 0 && fourBitBuffer[2] == 0 && fourBitBuffer[3] == 1 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '2':
+			if(fourBitBuffer[0] == 0 && fourBitBuffer[1] == 0 && fourBitBuffer[2] == 1 && fourBitBuffer[3] == 0 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '3':
+			if(fourBitBuffer[0] == 0 && fourBitBuffer[1] == 0 && fourBitBuffer[2] == 1 && fourBitBuffer[3] == 1 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '4':
+			if(fourBitBuffer[0] == 0 && fourBitBuffer[1] == 1 && fourBitBuffer[2] == 0 && fourBitBuffer[3] == 0 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '5':
+			if(fourBitBuffer[0] == 0 && fourBitBuffer[1] == 1 && fourBitBuffer[2] == 0 && fourBitBuffer[3] == 1 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '6':
+			if(fourBitBuffer[0] == 0 && fourBitBuffer[1] == 1 && fourBitBuffer[2] == 1 && fourBitBuffer[3] == 0 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '7':
+			if(fourBitBuffer[0] == 0 && fourBitBuffer[1] == 1 && fourBitBuffer[2] == 1 && fourBitBuffer[3] == 1 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '8':
+			if(fourBitBuffer[0] == 1 && fourBitBuffer[1] == 0 && fourBitBuffer[2] == 0 && fourBitBuffer[3] == 0 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case '9':
+			if(fourBitBuffer[0] == 1 && fourBitBuffer[1] == 0 && fourBitBuffer[2] == 0 && fourBitBuffer[3] == 1 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		//Checking letters
+		case 'A':
+			if(fourBitBuffer[0] == 1 && fourBitBuffer[1] == 0 && fourBitBuffer[2] == 1 && fourBitBuffer[3] == 0 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case 'B':
+			if(fourBitBuffer[0] == 1 && fourBitBuffer[1] == 0 && fourBitBuffer[2] == 1 && fourBitBuffer[3] == 1 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case 'C':
+			if(fourBitBuffer[0] == 1 && fourBitBuffer[1] == 1 && fourBitBuffer[2] == 0 && fourBitBuffer[3] == 0 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case 'D':
+			if(fourBitBuffer[0] == 1 && fourBitBuffer[1] == 1 && fourBitBuffer[2] == 0 && fourBitBuffer[3] == 1 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case 'E':
+			if(fourBitBuffer[0] == 1 && fourBitBuffer[1] == 1 && fourBitBuffer[2] == 1 && fourBitBuffer[3] == 0 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+		case 'F':
+			if(fourBitBuffer[0] == 1 && fourBitBuffer[1] == 1 && fourBitBuffer[2] == 1 && fourBitBuffer[3] == 1 ){
+				return true;
+				
+			}else{
+				return false;
+			}
+			break;
+	}
+	
+	return false;
 }
 
 
-
-
-
-
-
-
-
-LTimer::LTimer()
-{
-    //  Initialize the variables
-    mStartTicks = 0;
-    mPausedTicks= 0;
-
-    mPaused = false;
-    mStarted= false;
+bool checkIfBinaryNumberEqualsHex(int inputBinaryNumber[8],char inputHexData[2]){
+	bool isFirstHalfOfBinaryNumberCorrect;
+	bool isSecondHalfOfBinaryNumberCorrect;
+	
+	int fourBitBuffer[4];
+	int secondFourBitBuffer[4];
+	//Getting the first half of the binary number and storing it into an array
+	for (int i = 4, j = 0;i < 8,j < 4; i++,j++){
+		fourBitBuffer[j] = inputBinaryNumber[i];
+		cout << fourBitBuffer[j];
+	}
+	cout << " ";
+	//Getting the second half of the binary number and storing it into another array
+	
+	for (int i = 0;i <4; i++){
+		secondFourBitBuffer[i] = inputBinaryNumber[i];
+		cout << secondFourBitBuffer[i];
+	}
+	cout << endl;
+	//Since we are checking the first half of binary number, we need then second digit of the hex number, hence the second element
+	isFirstHalfOfBinaryNumberCorrect = check4BitEqualsHex(fourBitBuffer,inputHexData[1]);
+	isSecondHalfOfBinaryNumberCorrect = check4BitEqualsHex(secondFourBitBuffer,inputHexData[0]);
+	
+	if(isFirstHalfOfBinaryNumberCorrect==true && isSecondHalfOfBinaryNumberCorrect==true){
+		return true;
+	}else{
+		return false;
+	}
+	//If somehow we get here, but do this to avoid compiler warnings
+	cout << "Function reached a point it shouldn't have " << endl;
+	return false;
 }
 
-void LTimer::start()
-{
-    //  Start the timer
-    mStarted= true;
-
-    //  Unpause the timer
-    mPaused = false;
-
-    //  Get the current clock time
-    mStartTicks = SDL_GetTicks();
-	mPausedTicks= 0;
+char assignIntToHex(int inputNumber){
+	char returnChar;
+	if (inputNumber > 9){
+		switch(inputNumber){
+			case 10:
+				returnChar = 'A';
+				break;
+			
+			case 11:
+				returnChar = 'B';
+				break;
+			
+			case 12:
+				returnChar = 'C';
+				break;
+			
+			case 13:
+				returnChar = 'D';
+				break;
+			
+			case 14:
+				returnChar = 'E';
+				break;
+			
+			case 15:
+				returnChar = 'F';
+				break;
+		}
+	}else{
+		switch(inputNumber){
+			case 0:
+				returnChar = '0';
+				break;
+			case 1:
+				returnChar = '1';
+				break;
+			case 2:
+				returnChar = '2';
+				break;
+			case 3:
+				returnChar = '3';
+				break;
+			case 4:
+				returnChar = '4';
+				break;
+			case 5:
+				returnChar = '5';
+				break;
+			case 6:
+				returnChar = '6';
+				break;
+			case 7:
+				returnChar = '7';
+				break;
+			case 8:
+				returnChar = '8';
+				break;
+			case 9:
+				returnChar = '9';
+				break;
+		}
+	}
+	
+	return returnChar;
 }
 
-void LTimer::stop()
-{
-    //  Stop the timer
-    mStarted= false;
+void drawText(const char* inputText, int x, int y){
+	SDL_Surface* text;
+	// Set color to black
+	SDL_Color color = { 0, 0, 0 };
 
-    //Unpause the timer
-    mPaused = false;
+	text = TTF_RenderText_Solid( font, inputText, color );
+	if ( !text ) {
+		cout << "Failed to render text: " << TTF_GetError() << endl;
+	}
+	
+	SDL_Texture* text_texture;
 
-	//  Clear tick variables
-	mStartTicks = 0;
-	mPausedTicks= 0;
+	text_texture = SDL_CreateTextureFromSurface( renderer, text );
+
+	SDL_Rect dest = {x,y, text->w, text->h };
+
+	SDL_RenderCopy( renderer, text_texture, NULL,&dest );
 }
 
-void LTimer::pause()
-{
-    //  If the timer is running and isn't already paused
-    if  ( mStarted && !mPaused )
-    {
-        //  Pause the timer
-        mPaused = true;
-
-        //  Calculate the paused ticks
-        mPausedTicks= SDL_GetTicks() - mStartTicks;
-		mStartTicks = 0;
-    }
+void createMissile(){
+	Missile m1;
+	
+	m1.missileRect.w = 20;
+	m1.missileRect.h = 50;
+	m1.missileRect.x = enemyVector[0].getPosX();
+	m1.missileRect.y = 350;
+	
+	missileSpriteVector.push_back(m1);
 }
 
-void LTimer::unpause()
-{
-    //  If the timer is running and paused
-    if  ( mStarted && mPaused )
-    {
-        //  Unpause the timer
-        mPaused = false;
-
-        //  Reset the starting ticks
-        mStartTicks = SDL_GetTicks() - mPausedTicks;
-
-        //  Reset the paused ticks
-        mPausedTicks = 0;
-    }
+Uint32 addEnemy(Uint32, void*){
+	enemy enemyBuffer;
+	enemyVector.push_back(enemyBuffer);
+	return 10000;
 }
 
-Uint32 LTimer::getTicks()
-{
-	//  The actual timer time
-	Uint32 time = 0;
-
-    //  If the timer is running
-    if  ( mStarted )
-    {
-        //  If the timer is paused
-        if  ( mPaused )
-        {
-            //  Return the number of ticks when the timer was paused
-            time = mPausedTicks;
-        }
-        else
-        {
-            //  Return the current time minus the start time
-            time = SDL_GetTicks() - mStartTicks;
-        }
-    }
-
-    return time;
+void createEnemy(){
+	enemy enemyBuffer;
+	enemyVector.push_back(enemyBuffer);
 }
 
-bool LTimer::isStarted()
-{
-	//  Timer is running and paused or unpaused
-    return mStarted;
-}
 
-bool LTimer::isPaused()
-{
-	//  Timer is running and paused
-    return mPaused && mStarted;
-}
