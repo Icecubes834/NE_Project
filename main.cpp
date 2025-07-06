4bb9ed81b3f4c9177bc80b69dad2ae6d052e52b7
Icecubes834 <icecubes834@gmail.com>
2025-07-06 18:37:00 +0100

    3rd version of main.cpp
    More features were added with random hex number generation, but main functionality was not there.
    
    Created: 09/05/2025 13:26
    Last modified: 11/05/2025 19:08

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@
 #include <ctime>
 #include <unistd.h>
 #include <vector>
+#include <time.h>
+#include <sstream>
 
 using namespace std;
 
 bool init();
 void kill();
 bool loop();
+void changeState(int padNumber);
 
 // Pointers to our window and renderer
 SDL_Window* window;
@@ -20,6 +23,8 @@ const int WIDTH = 640;
 const int HEIGHT = 480;
 const int SCREEN_FPS = 60;
 const int SCREEN_TICK_PER_FRAME = 1000 / SCREEN_FPS;
+
+int binaryNumber[8] = {0,0,0,0,    0,0,0,0};
 
 
 
@@ -65,45 +70,70 @@ class enemy{
 		//Defines the shape of the enemy
 		SDL_Rect enemyRect;
 		//Shows the hex value it represents in a string format
-		string hexData;
+		char hexData[2];
+		
+		
 };
 
 
 class BinaryPad{
 	public:
-		BinaryPad(int startingX);
+		//Constructor with starting pos x and pad number as its parameters
+		BinaryPad(int startingX,int inputPadNumber);
+		//Function to draw the pad
 		void draw();
+		//Function to change the x pos
+		void changeX(int x);
+		
+		//Returns pad number
+		int getPadNumber(){
+			return padNumber;
+		}
+		
+		int getState(){
+			return state;
+		}
+
+		void setState(int inputState){
+			state  = inputState;
+		}
 	
 	private:
 		SDL_Rect padRect;
-		int state;
+		int state = 0;
+		//The pad number it represents
+		int padNumber;
 	
 };
 
 
-BinaryPad p1(50);
-BinaryPad p2(100);
-BinaryPad p3(200);
-BinaryPad p4(300);
+BinaryPad p1(0,0);
+BinaryPad p2(0,1);
+BinaryPad p3(0,2);
+BinaryPad p4(0,3);
 
-BinaryPad p5(400);
-BinaryPad p6(500);
-BinaryPad p7(600);
-BinaryPad p8(650);
+BinaryPad p5(0,4);
+BinaryPad p6(0,5);
+BinaryPad p7(0,6);
+BinaryPad p8(0,7);
 
 
 vector<enemy> enemyVector;
-vector<enemy> enemyVector;
+vector<BinaryPad> binaryPadVector;
+
+
 
 enemy e1;
 
 
 int main(int argc, char** args) {
-
+	srand(time(0));
+	
 	if ( !init() ) return 1;
+	
 	LTimer fpsTimer;
 	LTimer capTimer;
-	
+
 	int countedFrames = 0;
 	fpsTimer.start();
 
@@ -112,16 +142,15 @@ int main(int argc, char** args) {
 		capTimer.start();
 		
 		float avgFPS = countedFrames / ( fpsTimer.getTicks() / 1000.f );
-		if ( avgFPS > 2000000 )
-		{
+		if ( avgFPS > 2000000 ){
 			avgFPS = 0;
 		}
-		cout <<"FPS: " <<avgFPS<<"\r";
+		
+		//cout <<"FPS: " <<avgFPS<<"\r";
 		
 		
 		int frameTicks = capTimer.getTicks();
-        if  ( frameTicks < SCREEN_TICK_PER_FRAME )
-        {
+        if  ( frameTicks < SCREEN_TICK_PER_FRAME ){
             // Wait remaining time
 			SDL_Delay( SCREEN_TICK_PER_FRAME - frameTicks );
         }
@@ -152,14 +181,51 @@ bool loop() {
 		switch ( e.type ) {
 			case SDL_QUIT:
 				return false;
+				
+			case SDL_KEYDOWN:
+			// test keycode
+				switch ( e.key.keysym.sym ) {
+					case SDLK_1: 
+						changeState(0);
+						break;
+					case SDLK_2:
+						changeState(1);
+						break;
+					case SDLK_3: 
+						changeState(2);
+						break;
+					case SDLK_4:
+						changeState(3);
+						break;
+			
+					case SDLK_5: 
+						changeState(4);
+						break;
+					case SDLK_6:
+						changeState(5);
+						break;
+					case SDLK_7: 
+						changeState(6);
+						break;
+					case SDLK_8:
+						changeState(7);
+						break;
+						
+					case SDLK_SPACE:
+						enemy enemyBuffer;
+						enemyVector.push_back(enemyBuffer);
+						break;
+					
+				
+				}
+				break;
 		}
 	}
 
-	// Set drawing color to Red
-	SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+	
 
 	// Test key states - this could also be done with events
-	if ( keys[SDL_SCANCODE_A] ) r.x -= 2;
+	if ( keys[SDL_SCANCODE_2] ) r.x+=2;
 	
 	if ( keys[SDL_SCANCODE_D] ) r.x += 2;
 	
@@ -167,16 +233,17 @@ bool loop() {
 	
 	if ( keys[SDL_SCANCODE_S] ) r.y += 2;
 	
-	
-	
-	for (int i = 0; i < enemyVector.size(); i++) {
+	// Set drawing color to Red
+	SDL_SetRenderDrawColor( renderer,0, 255, 0, 255 );
+	for (uint8_t i = 0; i < enemyVector.size(); i++) {
 		enemyVector.at(i).draw();
 	}
 	
+	//SDL_SetRenderDrawColor( renderer, 0,0, 255, 255 );
 	
-	SDL_SetRenderDrawColor( renderer, 0,0, 255, 255 );
-	BinaryPad b1(50);
-	b1.draw();
+	for (int i = 0; i < 8; i++){
+		binaryPadVector.at(i).draw();
+	}
 	
 	// Update window
 	SDL_RenderPresent( renderer );
@@ -194,14 +261,22 @@ bool init() {
 	r.x = 0;
 	r.y = 0;
 	
-	SDL_Delay(2000);
-	enemy a;
-	SDL_Delay(2000);
-	enemy b;
-	
 	enemyVector.push_back(e1);
-	enemyVector.push_back(a);
-	enemyVector.push_back(b);
+	
+	binaryPadVector.push_back(p1);
+	binaryPadVector.push_back(p2);
+	binaryPadVector.push_back(p3);
+	binaryPadVector.push_back(p4);
+
+	binaryPadVector.push_back(p5);
+	binaryPadVector.push_back(p6);
+	binaryPadVector.push_back(p7);
+	binaryPadVector.push_back(p8);
+	
+	for(int i = 0; i < 8; i++){
+		//Setting the position of each binary pad to be equal spaces
+		binaryPadVector.at(i).changeX((i*80)+ 15);
+	}
 	
 	if ( SDL_Init( SDL_INIT_EVERYTHING ) < 0 ) {
 		cout << "Error initializing SDL: " << SDL_GetError() << endl;
@@ -249,37 +324,48 @@ void kill() {
 
 
 enemy::enemy(){
-	
-	
-	
 	//Defining the width and height of the enemy and starting position
 	enemyRect.w = 50;
 	enemyRect.h = 50;
 	
+	//Setting a seed based on time (for the random number generation)
+	//srand(time(0));
 	
-	srand((unsigned)time(NULL));
+	//Generate a random number
+	int randomStartXPosition = 140 + (rand() % WIDTH -140);
 	
-	cout <<"Here is another random number: "<< rand()<<endl;
-	int randomStartXPosition = (rand() % WIDTH);
-	
-	if(randomStartXPosition > enemyRect.w){
-		randomStartXPosition = (rand() % WIDTH) - enemyRect.w;  
-	}
 	cout <<"Random Number generated: "<< randomStartXPosition << endl;
 	
 	enemyRect.x = randomStartXPosition;
 	enemyRect.y = 0;
+	
+	int firstRandomHexDigit = rand()%16;
+	
+	cout <<"The random number is: "<<firstRandomHexDigit;
+	if (firstRandomHexDigit > 9){
+		if(firstRandomHexDigit == 10) hexData[0] = 'A';
+		if(firstRandomHexDigit == 11) hexData[0] = 'B';
+		if(firstRandomHexDigit == 12) hexData[0] = 'C';
+		if(firstRandomHexDigit == 13) hexData[0] = 'D';
+		if(firstRandomHexDigit == 14) hexData[0] = 'E';
+		if(firstRandomHexDigit == 15) hexData[0] = 'F';
+	}
+	cout << "Hex number"<<hexData;
 }
 
 void enemy::draw(){
 	enemyRect.y += 1;
+	
 	SDL_RenderFillRect(renderer,&enemyRect);
 }
 
 
-BinaryPad::BinaryPad(int startingX){
+
+
+
+
+BinaryPad::BinaryPad(int startingX,int inputPadNumber){
 	//Constructor
-	
 	
 	padRect.w = 50;
 	padRect.h = 50;
@@ -287,14 +373,43 @@ BinaryPad::BinaryPad(int startingX){
 	padRect.y = HEIGHT - (padRect.h + 20);
 	padRect.x = startingX;
 	
-	state = 0;
+	padNumber = inputPadNumber;
 	
 }
 
 void BinaryPad::draw(){
+	if (state == 1){
+		SDL_SetRenderDrawColor( renderer, 0, 0, 255, 255 );
+	}else{
+		SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+	}
 	SDL_RenderFillRect(renderer,&padRect);
 }
 
+void BinaryPad::changeX(int x){
+	padRect.x = x;
+}
+
+
+
+
+void changeState(int padNumber){
+	for (int i = 0;i < 8;i++){
+		if (padNumber==binaryPadVector.at(i).getPadNumber()){
+			if (binaryPadVector.at(i).getState() == 0){
+				binaryPadVector.at(i).setState(1);
+				binaryNumber[i] = 1;
+			}else{
+				binaryPadVector.at(i).setState(0);
+				binaryNumber[i] = 0;
+			}
+			cout << "Successfully changed state: " << binaryPadVector.at(i).getState() <<endl;
+			for (int i = 0;i < 8;i++){
+				cout << binaryNumber[i];
+			}
+		}
+	}
+}
 
 
 
